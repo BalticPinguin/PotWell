287936adc3fa18a482ca403272208df04380e3e6
BalticPinguin <hu.bert@xemail.me>
2017-05-05 17:07:35 +0200

    found a working configuration.

--- TestInf.C
+++ TestInf.C
@@ -90,8 +90,7 @@ using namespace libMesh;
 
 //prototypes of functions needed to set-up the system:
 void assemble_SchroedingerEquation(libMesh::EquationSystems & , const std::string &);
-void get_dirichlet_dofs(EquationSystems &, const std::string & , std::set<unsigned int>&);
-void cube_io(EquationSystems& es, std::string output, std::string SysName);
+void line_print(EquationSystems& es, std::string output, std::string SysName);
 
 int main (int argc, char** argv){
    // Initialize libMesh and the dependent libraries.
@@ -130,38 +129,34 @@ int main (int argc, char** argv){
    }
 
    // Skip this example if libMesh was compiled with <3 dimensions.
-   //  INFINITE ELEMENTS ARE IMPLIMENTED ONLY FOR 3 DIMENSIONS AT THE MOMENT.
+   // INFINITE ELEMENTS ARE IMPLEMENTED ONLY FOR 3 DIMENSIONS AT THE MOMENT.
    libmesh_example_requires(3 <= LIBMESH_DIM, "3D support");
    
    int dim = 3;
    // Create two different meshes; to one of them infinite elements will be added
    // and finally the results of the calculations can be compared.
    Mesh mesh(init.comm(), dim);
-   Mesh inf_mesh(init.comm(), dim);
    
    // overwrite the meshes with a spherical grid with radius 1.7
-   Real E = cl("Energy", 0.0);
-   Real r=cl("radius", 20.);
-   int NrBall=cl("points", 50);
-   Real VolConst= cl("maxVol", 1./(32.*sqrt(E*E*E)) );
-   Real L=cl("bending", 2.);
-   int N=cl("circles", 5);
+   Real E = cl("Energy", 0.1);
+   Real r=cl("radius", 2.);
    unsigned int maxiter=cl("maxiter", 700);
 
-   MeshTools::Generation::build_sphere (mesh, r, 3, HEX8,
-                                         3, true);
-   MeshTools::Generation::build_sphere (inf_mesh, r, 3, HEX8,
-                                         3, true);
+   //MeshTools::Generation::build_sphere (mesh, r, 3, HEX27, 3, true);
+   //MeshTools::Generation::build_sphere (mesh, r, 1, HEX8, 1, true);
+   MeshTools::Generation::build_sphere (mesh, r, 2, HEX8, 2, true);
 
-
+   //FEType fe_type(SECOND, LAGRANGE, FIFTH, JACOBI_20_00, CARTESIAN);
+   FEType fe_type(FIRST, LAGRANGE, FIFTH, JACOBI_20_00, CARTESIAN);
    //In case of infinite elements, they are added now by respective interface
-   InfElemBuilder builder(inf_mesh);
+
+   InfElemBuilder builder(mesh);
    builder.build_inf_elem(true);
    
    // Reassign subdomain_id() of all infinite elements.
    // Otherwise, the exodus-api will fail.
-   MeshBase::element_iterator       elem_it  = inf_mesh.elements_begin();
-   const MeshBase::element_iterator elem_end = inf_mesh.elements_end();
+   MeshBase::element_iterator       elem_it  = mesh.elements_begin();
+   const MeshBase::element_iterator elem_end = mesh.elements_end();
    for (; elem_it != elem_end; ++elem_it){
       Elem* elem = *elem_it;
       if(elem->infinite()){
@@ -169,193 +164,86 @@ int main (int argc, char** argv){
       }
    }
 
+   // find the neighbours; for correct linking the two areas
+   mesh.find_neighbors();
+
+   // convert all elements from 1-st order to second order elements.
+   //mesh.all_second_order();
+
    // Create an equation systems object for both systems.
    // Infinite elements don't need Condensed system in principle.
-   EquationSystems finite_eq_sys (mesh);
-   EquationSystems infinite_eq_sys (inf_mesh);
+   EquationSystems eq_sys (mesh);
    
-   CondensedEigenSystem & finite_eig_sys = finite_eq_sys.add_system<CondensedEigenSystem> ("EigenSE");
-   CondensedEigenSystem & infinite_eig_sys = infinite_eq_sys.add_system<CondensedEigenSystem> ("EigenSE");
+   CondensedEigenSystem & eig_sys = eq_sys.add_system<CondensedEigenSystem> ("EigenSE");
 
    // will be approximated using first-order approximation.
-   finite_eig_sys.add_variable("phi", FIRST);
-   infinite_eig_sys.add_variable("phi2", FIRST);
+   eig_sys.add_variable("phi", fe_type);
 
    // assign the ground state energy. For infinite elements, this guess should be 
    // good; otherwise the long-range limit will be wrong.
-   finite_eq_sys.parameters.set<Number>("gsE")=E;
-   infinite_eq_sys.parameters.set<Number>("gsE")=E;
+   eq_sys.parameters.set<Number>("gsE")=E;
    
    // set numerical parameters for SLEPC on how to solve the system.
-   finite_eig_sys.eigen_solver->set_eigensolver_type(KRYLOVSCHUR); // this is default
-   infinite_eig_sys.eigen_solver->set_eigensolver_type(KRYLOVSCHUR);
+   eig_sys.eigen_solver->set_eigensolver_type(KRYLOVSCHUR); // this is default
 
-   finite_eig_sys.eigen_solver->set_position_of_spectrum( 0.0);
-   infinite_eig_sys.eigen_solver->set_position_of_spectrum(0.0);
+   eig_sys.eigen_solver->set_position_of_spectrum(E);
    
-   SlepcEigenSolver<Number>* fin_solver = 
-                 libmesh_cast_ptr<SlepcEigenSolver<Number>* >( &(*finite_eig_sys.eigen_solver) );
-   SlepcEigenSolver<Number>* inf_solver = 
-                 libmesh_cast_ptr<SlepcEigenSolver<Number>* >( &(*infinite_eig_sys.eigen_solver) );
+   SlepcEigenSolver<Number>* solver = 
+                 libmesh_cast_ptr<SlepcEigenSolver<Number>* >( &(*eig_sys.eigen_solver) );
 
-   SlepcSolverConfiguration FConfigSolver( *fin_solver);
-   SlepcSolverConfiguration IConfigSolver( *inf_solver);
+   SlepcSolverConfiguration ConfigSolver( *solver);
 
    // set the spectral transformation:
-   IConfigSolver.SetST(SINVERT);
-   FConfigSolver.SetST(SINVERT);
+   ConfigSolver.SetST(SINVERT);
    //ConfigSolver.SetST(CAYLEY);
    //ConfigSolver.SetST(SHIFT); // this is default
-   fin_solver ->set_solver_configuration(FConfigSolver);
-   inf_solver ->set_solver_configuration(IConfigSolver);
+   solver ->set_solver_configuration(ConfigSolver);
 
-   finite_eq_sys.parameters.set<Real>("radius")    = r;
-   infinite_eq_sys.parameters.set<Real>("radius")    = r;
+   eq_sys.parameters.set<Real>("radius")    = r;
    //set number of eigen values ( \p nev) and number of 
    // basis vectors \p ncv for the solution.
    //Note that ncv >= nev must hold and ncv >= 2*nev is recommended.
-   finite_eq_sys.parameters.set<unsigned int>("eigenpairs")    = nev;
-   finite_eq_sys.parameters.set<unsigned int>("basis vectors") = nev*3+4;
-   infinite_eq_sys.parameters.set<unsigned int>("eigenpairs")    = nev;
-   infinite_eq_sys.parameters.set<unsigned int>("basis vectors") = nev*3+4;
+   eq_sys.parameters.set<unsigned int>("eigenpairs")    = nev;
+   eq_sys.parameters.set<unsigned int>("basis vectors") = nev*3+4;
  
    // attach the name of the function that assembles the matrix equation:
-   finite_eig_sys.attach_assemble_function (assemble_SchroedingerEquation);
-   infinite_eig_sys.attach_assemble_function (assemble_SchroedingerEquation);
+   eig_sys.attach_assemble_function (assemble_SchroedingerEquation);
   
-   // important to set the system to be generalised hermitian eigen problem.
+   // important to set the system to be generalised nonhermitian eigen problem.
    // By default it is HEP and so _matrix_B is not available.
-   finite_eig_sys.set_eigenproblem_type(GHEP);
-   infinite_eig_sys.set_eigenproblem_type(GHEP);
+   eig_sys.set_eigenproblem_type(GNHEP);
    
    // Set the solver tolerance and the maximum number of iterations.
-   finite_eq_sys.parameters.set<Real> ("linear solver tolerance") = pow(TOLERANCE, 5./3.);
-   finite_eq_sys.parameters.set<unsigned int>("linear solver maximum iterations") = maxiter;
-   infinite_eq_sys.parameters.set<Real> ("linear solver tolerance") = pow(TOLERANCE, 5./3.);
-   infinite_eq_sys.parameters.set<unsigned int>("linear solver maximum iterations") = maxiter*2;
+   eq_sys.parameters.set<Real> ("linear solver tolerance") = pow(TOLERANCE, 5./3.);
+   eq_sys.parameters.set<unsigned int>("linear solver maximum iterations") = maxiter;
 
    // Initialize the data structures for the equation system.
-   finite_eq_sys.init();
-   infinite_eq_sys.init();
-   
-   // if no infinite elements are used, we need some boundary condition.
-   // Here: force the wave function to vanish at all boarders.
-   std::set<unsigned int> dirichlet_dof_ids;
-   get_dirichlet_dofs(finite_eq_sys, "EigenSE" ,dirichlet_dof_ids);
-   finite_eig_sys.initialize_condensed_dofs(dirichlet_dof_ids);
+   eq_sys.init();
 
-   // Solve both systems. In this function, the assemble-functions are called.
-   finite_eig_sys.solve();
-   infinite_eig_sys.solve();
+   // Solve system. In this function, the assemble-functions are called.
+   eig_sys.solve();
    
    // get number of converged eigenpairs
-   unsigned int fnconv = finite_eig_sys.get_n_converged();
-   unsigned int inconv = infinite_eig_sys.get_n_converged();
+   unsigned int nconv = eig_sys.get_n_converged();
 
-   out << "Number of converged eigenpairs: " << fnconv << "\n";
-   out << "Number of converged eigenpairs: " << inconv << "\n" << std::endl;
+   out << "Number of converged eigenpairs: " << nconv << "\n";
 
    // Write the eigen vector to file and the eigenvalues to libMesh::out.
-   out<<"energy of state   (without infinite)"<<std::endl;
-   for(unsigned int i=0; i<fnconv; i++){
-      std::pair<Real,Real> eigpair = finite_eig_sys.get_eigenpair(i);
-      out<<"                   ";
-      out<<eigpair.first+finite_eq_sys.parameters.set<Number>("gsE")<<std::endl;
-      std::ostringstream eigenvector_output_name;
-      eigenvector_output_name<< i <<".e" ;
-      #ifdef LIBMESH_HAVE_EXODUS_API
-          ExodusII_IO (mesh).write_equation_systems ( eigenvector_output_name.str(), finite_eq_sys);
-      #endif // #ifdef LIBMESH_HAVE_EXODUS_API
-      std::ostringstream fe_file;
-      fe_file<<"finite_"<<i<<".cube";
-      //eigenvector_output_name<< i <<".vtk" ;
-      //VTKIO(mesh).write_equation_systems ( eigenvector_output_name.str(), finite_eq_sys);
-   }
-   for(unsigned int i=0; i<inconv; i++){
-      std::pair<Real,Real> eigpair = infinite_eig_sys.get_eigenpair(i);
-      std::ostringstream eigenvector_output_name;
-      eigpair = infinite_eig_sys.get_eigenpair(i);
-      out<<"        "<<eigpair.first+infinite_eq_sys.parameters.set<Number>("gsE")<<std::endl;
-      eigenvector_output_name<< i <<"_inf.e" ;
-      #ifdef LIBMESH_HAVE_EXODUS_API
-         ExodusII_IO (inf_mesh).write_equation_systems (eigenvector_output_name.str(), infinite_eq_sys);
-      #endif // #ifdef LIBMESH_HAVE_EXODUS_API
-      std::ostringstream inf_file;
-      inf_file<<"infini_"<<i<<".cube";
-      cube_io(infinite_eq_sys, inf_file.str(), "EigenSE");
-      //eigenvector_output_name<< i <<"_inf.vtk";
-      //VTKIO(mesh).write_equation_systems ( eigenvector_output_name.str(), finite_eq_sys);
+   for(unsigned int i=0; i<nconv; i++){
+      std::pair<Real,Real> eigpair = eig_sys.get_eigenpair(i);
+      eigpair = eig_sys.get_eigenpair(i);
+      out<<"        "<<eigpair.first<<std::endl;
+      eq_sys.parameters.set<Real>("current frequency")=eq_sys.parameters.get<Real>("speed")*sqrt(std::abs(eigpair.first)*2.)/(2*pi);
+      
+      std::ostringstream file;
+      file<<"infini_"<<i<<".txt";
+      // print the solution along the x-coordinate
+      line_print(eq_sys, file.str(), "EigenSE");
    }
 
    // All done.
    return 0;
 #endif // LIBMESH_HAVE_SLEPC
-}
-
-void get_dirichlet_dofs(EquationSystems & es, const std::string & system_name, std::set<unsigned int>& dirichlet_dof_ids){
-   // This function is just copied from example...
-   dirichlet_dof_ids.clear();
-   // It is a good idea to make sure we are assembling
-   // the proper system.
-   libmesh_assert_equal_to (system_name, "EigenSE");
-   
-   // Get a constant reference to the mesh object.
-   const MeshBase& mesh = es.get_mesh();
-   
-   // The dimension that we are running.
-   const unsigned int dim = mesh.mesh_dimension();
-   
-   // Get a reference to our system.
-   CondensedEigenSystem & eigen_system = es.get_system<CondensedEigenSystem> (system_name);
-   
-   // Get a constant reference to the Finite Element type
-   // for the first (and only) variable in the system.
-   FEType fe_type = eigen_system.get_dof_map().variable_type(0);
-   const DofMap& dof_map = eigen_system.get_dof_map();
-   
-   // This vector will hold the degree of freedom indices for
-   // the element.  These define where in the global system
-   // the element degrees of freedom get mapped.
-   std::vector<dof_id_type> dof_indices;
-   
-   // Now we will loop over all the elements in the mesh that
-   // live on the local processor. We will compute the element
-   // matrix and right-hand-side contribution.  In case users
-   // later modify this program to include refinement, we will
-   // be safe and will only consider the active elements;
-   // hence we use a variant of the \p active_elem_iterator.
-   MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
-   const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
-   
-   for ( ; el != end_el; ++el){
-      // Store a pointer to the element we are currently
-      // working on.  This allows for nicer syntax later.
-      const Elem* elem = *el;
-
-      // Get the degree of freedom indices for the
-      // current element.  These define where in the global
-      // matrix and right-hand-side this element will
-      // contribute to.
-      dof_map.dof_indices (elem, dof_indices);
-      {
-        // All boundary dofs are Dirichlet dofs in this case
-        for (unsigned int s=0; s< elem->n_sides(); s++){
-            if (elem->neighbor(s) == NULL){
-               std::vector<unsigned int> side_dofs;
-               FEInterface::dofs_on_side(elem, dim, fe_type, s, side_dofs);
-
-               for(unsigned int ii=0; ii<side_dofs.size(); ii++){
-                  dirichlet_dof_ids.insert(dof_indices[side_dofs[ii]]);
-               }
-            }
-         }
-      }
-   } // end of element loop
-   
-   /**
-   * All done!
-   */
-   return;
 }
 
 void assemble_SchroedingerEquation(libMesh::EquationSystems & es, const std::string & system_name){
@@ -382,7 +270,6 @@ void assemble_SchroedingerEquation(libMesh::EquationSystems & es, const std::str
    // store the object as an \p UniquePtr<FEBase>.  This can be thought
    // of as a pointer that will clean up after itself.
    UniquePtr<FEBase> fe (FEBase::build(dim, fe_type));  // here, try AutoPtr instead...
-   //AutoPtr<FEBase> inf_fe (FEBase::build_InfFE(dim,fe_type));
    UniquePtr<FEBase> inf_fe (FEBase::build_InfFE(dim, fe_type));
    
    // A  Gauss quadrature rule for numerical integration.
@@ -393,16 +280,15 @@ void assemble_SchroedingerEquation(libMesh::EquationSystems & es, const std::str
    fe->attach_quadrature_rule (&qrule);
    inf_fe->attach_quadrature_rule (&qrule);
       
-   libMesh::Number co0_5= 0.5;
    libMesh::Number co2= 2.;
    Number E=es.parameters.get<Number>("gsE");  
    //libMesh::Number k=omega; //divided by c which is 0 in atomic units.
    // -->ik = -i*k => for neg. energy: exp(-i*sqrt(2E)*mu(x))= exp(-sqrt(2|E|)*mu(x)) ==> expon. decay in function.
    libMesh::Number ik=sqrt(-co2*E); // -->try this for now...
    // set parameters for infinite elements:
-   es.parameters.set<Real>("speed")=1.;
+   es.parameters.set<Real>("speed")=137.0359991;
    // --> it would be better if 'current frequency' could be <Number>, not <Real>.
-   es.parameters.set<Real>("current frequency")=abs(sqrt(co2*E));
+   es.parameters.set<Real>("current frequency")=es.parameters.get<Real>("speed")*sqrt(std::abs(E)*2.)/(2*pi);
 
    Number potval;  
    libMesh::Number temp; // -->try this for now...
@@ -492,7 +378,7 @@ void assemble_SchroedingerEquation(libMesh::EquationSystems & es, const std::str
          unsigned int n_sf = cfe->n_shape_functions();
          // loop over them:
          if (q_point[qp].size()<1.)
-            potval=-2.0;
+            potval=-0.5;
          else
             potval=0.0; // is assumed to be close enough to infinity
          for (unsigned int i=0; i<n_sf; i++){
@@ -501,8 +387,9 @@ void assemble_SchroedingerEquation(libMesh::EquationSystems & es, const std::str
                Se(i,j) += JxW[qp]*weight[qp]*phi[i][qp]*phi[j][qp];
                temp= dweight[qp]*phi[i][qp]*(dphi[j][qp]-ik*dphase[qp]*phi[j][qp])+
                      weight[qp]*(dphi[j][qp]*dphi[i][qp]-ik*ik*dphase[qp]*dphase[qp]*phi[i][qp]*phi[j][qp]+
+
                      ik*dphase[qp]*(phi[i][qp]*dphi[j][qp]-phi[j][qp]*dphi[i][qp]));
-               H(i,j) += JxW[qp]*(co0_5*temp + (potval- E)*weight[qp]*phi[i][qp]*phi[j][qp]);
+               H(i,j) += JxW[qp]*(0.5*temp + potval*weight[qp]*phi[i][qp]*phi[j][qp]);
             }
          }
       }
@@ -576,7 +463,7 @@ void SlepcSolverConfiguration::configure_solver()
    }
 }
 
-void cube_io(EquationSystems& es, std::string output, std::string SysName){
+void line_print(EquationSystems& es, std::string output, std::string SysName){
    //CondensedEigenSystem & system = es.get_system<CondensedEigenSystem> ("EigenSE"); // --> how to generalise??
    System & system = es.get_system<System> (SysName); 
    const MeshBase & mesh = es.get_mesh();
@@ -614,26 +501,20 @@ void cube_io(EquationSystems& es, std::string output, std::string SysName){
    unsigned int num_line=0;
    Real N = 100.;
    Point q_point;
-   for (int pts=1;pts<=4*N;pts++) {
-      if (pts%4==0){
-         q_point = Point(  pts*r/(4.*N), 0., 0.);
-         }
-      else if (pts%4==1){
-         q_point = Point( -pts*r/(4.*N), 0., 0.);
-         }
-      else if (pts%4==2){
-         q_point = Point ( -N*4./(pts*r), 0., 0.);
-         }
-      else {
-         q_point = Point (  N*4./(pts*r), 0., 0.);
-         }
+   const Real start=-2*r;
+   for (int pts=1;pts<=2*N;pts++) {
+      // go from -2*r to 2*r.
+      q_point = Point( start+ pts*r/N*2, 0., 0.);
       num_line++;
       
       const Elem * elem=pt_lctr(q_point);
       if(elem==NULL){
-         abs_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0;
-         im_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0;
-         re_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0;
+         re_out<<" "<<std::setw(12)<<q_point(0);
+         im_out<<" "<<std::setw(12)<<q_point(0);
+         abs_out<<" "<<std::setw(12)<<q_point(0);
+         abs_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0<<std::endl;
+         im_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0<<std::endl;
+         re_out<<" "<<std::setw(12)<<std::scientific<<std::setprecision(6)<<0.0<<std::endl;
       }
       else{
 
